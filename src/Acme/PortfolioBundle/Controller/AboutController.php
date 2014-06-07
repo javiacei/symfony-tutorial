a65127f25a3706a4aceed0420ce9cc650e16f754
nass600 <ivelazquez85@gmail.com>
2014-06-07 14:29:48 +0200

    Consuming github API in About section

--- src/Acme/PortfolioBundle/Controller/AboutController.php
+++ src/Acme/PortfolioBundle/Controller/AboutController.php
@@ -3,6 +3,7 @@
 namespace Acme\PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -12,14 +13,36 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class AboutController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $githubUrl = "https://api.github.com/users/symfony/repos";
+        $projects = $this->curl($githubUrl, $request);
+
         return $this->render(
             'AcmePortfolioBundle:About:index.html.twig',
             array(
-                'section' => 'About'
+                'section' => 'About',
+                'projects' => $projects
             )
         );
     }
+
+    protected function curl($url, Request $request)
+        {
+            $ch = curl_init($url);
+            $options = array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_USERAGENT => $request->headers->get('User-Agent')
+            );
+            curl_setopt_array($ch, $options);
+            $data = curl_exec($ch);
+
+            $headers = curl_getinfo($ch);
+
+            curl_close($ch);
+            return json_decode($data);
+        }
+
 }
  
