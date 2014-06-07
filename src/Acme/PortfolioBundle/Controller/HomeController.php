1fd782de6a8f899803bf9127214612178bf4f012
nass600 <ivelazquez85@gmail.com>
2014-06-07 19:05:43 +0200

    Added alert about cookies in Home

--- src/Acme/PortfolioBundle/Controller/HomeController.php
+++ src/Acme/PortfolioBundle/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace Acme\PortfolioBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -14,12 +15,32 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
+        $session = $this->get('session');
+
+        if (false == $session->get('cookies_accepted')) {
+            $session->getFlashBag()->add(
+                'success',
+                'By removing this alert you are accepting the following Cookie Policy'
+            );
+        }
+
         return $this->render(
             'AcmePortfolioBundle:Home:index.html.twig',
             array(
                 'section' => 'Home'
             )
         );
+    }
+
+    public function acceptCookiesAction(Request $request)
+    {
+        $this->get('session')->set('cookies_accepted', true);
+
+        if (false == $request->isXmlHttpRequest()) {
+            return $this->redirect($this->generateUrl('portfolio_home_index'));
+        }
+
+        return new Response();
     }
 }
  
