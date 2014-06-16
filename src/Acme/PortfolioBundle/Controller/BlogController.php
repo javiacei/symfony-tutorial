722af8f7c57f62533b022dc288a3d6c03b72d132
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2014-06-16 21:53:58 +0200

    FindOneBy Post

--- src/Acme/PortfolioBundle/Controller/BlogController.php
+++ src/Acme/PortfolioBundle/Controller/BlogController.php
@@ -68,7 +68,7 @@ class BlogController extends Controller
      *
      * @Template()
      * @Route("/{locale}/{slug}", name="portfolio_blog_show", requirements={
-     *        "locale" = "en|es",
+     *        "locale" = "[a-z\-]{2}",
      *        "slug" = "[a-z\-]+"
      * })
      *
@@ -79,27 +79,23 @@ class BlogController extends Controller
      */
     public function showAction($locale, $slug)
     {
-        $posts = $this->getPosts();
-
-        foreach($posts as $post) {
-            if ($post->getLocale() === $locale && $post->getSlug() === $slug) {
-                return array(
-                    'section' => 'Blog',
-                    'locale'  => $locale,
-                    'post'   => $post
-                );
-            }
-        }
-
-        throw $this->createNotFoundException("Post with slug '" . $slug . "' not found");
-    }
-
-    public function getPosts()
-    {
-        return $this
+        $post = $this
             ->get('doctrine.orm.default_entity_manager')
             ->getRepository('Acme\PortfolioBundle\Entity\Post')
-            ->findAll();
+            ->findOneBy(array(
+                'locale' => $locale,
+                'slug' => $slug
+            ));
+
+        if (null === $post) {
+            throw $this->createNotFoundException("Post with slug '" . $slug . "' not found");
+        }
+
+        return array(
+            'section' => 'Blog',
+            'locale'  => $locale,
+            'post'   => $post
+        );
     }
 }
  
