1703ec7132cd30d304a6e9cbe14def8692db8d63
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2014-06-21 17:06:32 +0200

    Create a new Post using FormType

--- src/Acme/AdminBundle/Controller/PostsController.php
+++ src/Acme/AdminBundle/Controller/PostsController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use Acme\AdminBundle\Form\PostType;
+use Acme\PortfolioBundle\Entity\Post;
+
 /**
  * @Route("/posts")
  */
@@ -39,8 +44,31 @@ class PostsController extends Controller
      * @Route("/create", name="admin_posts_create")
      * @Template()
      */
-    public function createAction()
+    public function createAction(Request $request)
     {
-        return array();
+        // Create new Post
+        $post = new Post();
+
+        $form = $this->createForm(new PostType(), $post);
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            // Save into database
+            $em = $this->get('doctrine.orm.default_entity_manager');
+
+            $em->persist($post);
+            $em->flush();
+
+            // Inform user that a new Post has been created
+            $this->get('session')->getFlashBag()->add('success', 'You have successfully create a new Post');
+
+            // Redirect to list of posts
+            return $this->redirect($this->generateUrl('admin_posts_list'));
+        }
+
+        return $this->render('AcmeAdminBundle:Posts:create.html.twig', array(
+            'form'=> $form->createView()
+        ));
     }
 }
