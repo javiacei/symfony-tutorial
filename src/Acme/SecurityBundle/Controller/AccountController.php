a8c2986aa11988509a8319b6c17df2a75d53889c
Fco Javier Aceituno Lapido <fco.javier.aceituno@gmail.com>
2014-06-30 17:12:31 +0200

    Login implementation

--- src/Acme/SecurityBundle/Controller/AccountController.php
+++ src/Acme/SecurityBundle/Controller/AccountController.php
@@ -3,6 +3,8 @@
 namespace Acme\SecurityBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
@@ -11,8 +13,28 @@ class AccountController extends Controller
     /**
      * @Template()
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        return array();
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        }
+
+        // last username entered by the user
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+        return array(
+            // last username entered by the user
+            'last_username' => $lastUsername,
+            'error'         => isset($error) ? $error : null,
+        );
+
     }
 }
